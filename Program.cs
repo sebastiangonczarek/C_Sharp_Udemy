cc45c01c4aeb5e6b8846a69fe7cd01bcae26442c
Sebastian Gonczarek <sebastian.gonczarek@gmail.com>
2020-11-07 22:03:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_Sharp_Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string string1 = "Dies ist ein String";
+            Console.WriteLine(string1.Length);
+            Console.ReadKey();
+        }
+    }
+}
